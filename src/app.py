9dfb1127659c6051ea5829d7a313ad5eebeee116
Anoop Babu <anoop.bab@gmail.com>
2020-03-08 01:25:41 -0500

    Merged w/ Jim, Sherzod, && Me

--- src/app.py
+++ src/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, request, render_template, send_from_directory
 from pytube import YouTube
+import requests as req
+import os
+import math
+from ebaysdk.finding import Connection as finding
+from bs4 import BeautifulSoup
+import base64
+
 import sys
 sys.path.append('../')
-
 import process
-import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+ID_APP = 'AhsanWah-MyApp-PRD-e69e8f5f8-f5a417a4'
+empty_url_b64 = "/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAYEBAQFBAYFBQYJBgUGCQsIBgYICwwKCgsKCgwQDAwMDAwMEAwODxAPDgwTExQUExMcGxsbHCAgICAgICAgICD/2wBDAQcHBw0MDRgQEBgaFREVGiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICD/wAARCABQAFADAREAAhEBAxEB/8QAHAABAAEFAQEAAAAAAAAAAAAAAAUBAgQGBwMI/8QAPBAAAQMDAQQFBwoHAAAAAAAAAQIDBAAFERIGEyExByIykaE0QUJRYXGBFBUXQ1JidZOisSMzNVNUcpL/xAAUAQEAAAAAAAAAAAAAAAAAAAAA/8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8A+qaBQKBQKBQKBQKBQKBQa7eNoEWuO9MmPSNyJXyZtEdLJPY1jtj2Hz0EN9Jdl+3cfy4tA+kuy/buP5cWgyYm3EWYQIzd0czwBDUXHeeFBLSrk/GgqmuialhsanApMcLHDzJxxNBAfSXZft3H8uLQbRbJq5LjS0urXHkRW5LYdCAobzlnQB5qCToFAoNH2xhSLlbFxISd9INw3ugc93uinV7smg1hexka3Nh7aG6R7c2eIStaQpQ+6DxJ9mmgjZO3PRtZ8i3w3r1ITycUNyzkefK8rHwTQQF06ZNrJIU3bgxaWDwxGQC4R95xeo59oxQXdGO0s53bMM3OW5J+dmlxFOPrK+ueu0etn00gfGgz7lEMSc/HIxu1EAHnjmPCg7Ds35PbvwuN+1BPUHhOcebiOuM/zEpyM8eXPwoOcdIHSS1s1OTAVCeuMlxpL7brzgaj6VZAKUoyVciOOKDmd36WdtLgktNSk22OfqYKdz+vi5+qg1F5555xTry1OOr4qWslSifaTQWUCg9Ykl6JKZlMK0vMLS60r1KQdQPeKDsO2G5lrhXuOMR7owh8D1FY1cT68kj4UHR9m/J7d+Fxv2oJ6goQCMHiDzFBx3ptsZcsMK5JGXbW+qG8fOWnOs2fcMDvoOL0CgUCgUHVdkZXzx0cPRCcybE8dPH6h3LiSfjvAKDrGzfk9u/C437UE9QKDXNrrKm7W242oj+pRSGif8hnrNnvx3UHywpKkqKVDChwIPMGgpQKBQe0SHMmPBiIw5IfV2Wmklaj7kpyaDrnRRsLthbZst+4Q/ktumxlNLQ8oBesEKbVuxk8CMdbHOg6jZWkNKisoOpLUBlsE8+odPHuoJigUGJcwQwmQntR1BzgOOB2h8RQfNXSlZfmnba4NoTiPKV8rj45FL/WOPYF6hQQNsst3uru6tsN6W55wyhS8e8jgPjQb3ZOgna2bpXcXGbY0eYUd87/AMI6vesUG/2ToO2OgaVzd7c3hz3qtDefYhvHiTQb1b7XbbazuLfFaiM/22UJQPjpxQZVBGRGlN3iQn0d3lv3KVqP6iaCToFBRSUqSUq4pUMEe+g15eyezl3dZk3aA1MmW8GMhTuVJ0pOQCjsq4H0hQT0ePHjNJZjtIZZT2W20hKR7gOFB6UFCQBknAHM0GKq5xc6WtUhQ9FoavHs+NBQqujvZQiOnPNR1qx68Dq+NBfHh7t3fuOqeeKdBUcAYzngBQZNAoFBhKcRGuCy4oIakI1alEAa2+B7wRQVNybV5O2uR5soHVz/ALHAoGLo7zUiKn1J/iL7zhPhQVFsjE6ntT6gcguq1eHZ8KDKSlKRpSAlI5AUFaBQKBQKCxxlpzG8QleOWoA4oL6BQKBQKBQKD//Z"
 
 videos = []
 
@@ -18,23 +25,71 @@ def index():
 @app.route("/other", methods=['PUT', 'POST'])
 def other():
     if request.method == 'POST':
-        yt = YouTube(request.data.decode("utf-8"))
+        yt = YouTube(request.values['url'])
         videos.append(yt.streams.filter(resolution='720p', file_extension='mp4')[0].download())
         print(videos)
+        description = request.values['description']
     else:
         filename = request.files['video'].filename
         request.files['video'].save(filename)
         process.video_to_frames(filename, f"{filename.split('.')[0]}/")
-        os.remove(filename)
-        # videos.append(request.files['video'])
 
-    return "Success"
+        # videos.append(request.files['video'])
+        description = request.values['description']
+    return {} 
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='./static/favicon.icon')
-                               
+
+@app.route("/get_ebay_products", methods=['POST'])
+def get_ebay_products():
+    def get_items(listOfItems):
+        Keywords = listOfItems
+        api = finding(appid=ID_APP, config_file=None)
+        api_request = { 'keywords': Keywords }
+        response = api.execute('findItemsByKeywords', api_request)
+        soup = BeautifulSoup(response.content,'lxml')
+        totalentries = int(soup.find('totalentries').text)
+        items = soup.find_all('item')
+        return items
+
+    all_values = request.values['list'].split("-")
+    display = []
+    for value in all_values:    
+        values = value.strip()
+        if values == '':
+            continue
+        if values.find("size") > 0:
+            values = values[0 : values.find("size") - 1]
+        else:
+            s = " "
+            values = s.join(values.split(" ")[0 : math.ceil((len(values) + 1) / 2)])
+
+        items = get_items(values)
+        count = 0
+        for item in items:
+            if count == 3:
+                break
+            count += 1
+            title = item.title.string
+            price = int(round(float(item.currentprice.string)))
+            url = item.galleryurl.string
+            print(item)
+            link = item.viewitemurl.string
+            my_string = base64.b64encode(req.get(url).content)
+            if my_string.decode("utf-8") == empty_url_b64:
+                continue
+
+            display.append({
+                'title' : title,
+                'url' : url,
+                'price' : price,
+                'link' : link
+            })
+
+    return {'list' : display}
 
 if __name__ == '__main__':
     app.run()
--- src/static/index.js
+++ src/static/index.js
@@ -13,39 +13,58 @@
         // Initialize Firebase
         firebase.initializeApp(firebaseConfig);    
 
+        google()
+
         $(document).on("change", ".file_multi_video", function(evt) {
             var $source = $('#video_here');
             $source[0].src = URL.createObjectURL(this.files[0]);
             $source.parent()[0].load();
-
-            const fileToUpload = this.files[0]
-            console.log(fileToUpload)
-            var fd = new FormData();
-            fd.append('video', fileToUpload)
-            $.ajax({
-                url: '/other',
-                type: 'PUT',
-                data: fd,
-                contentType: false,
-                processData: false,
-                success: function(result) {
-                    // Do something with the result,
-                    console.log(result)
-                },
-                error: function(e) {
-                    console.log(e);
-                },
-            });
         });
+
+        document.getElementById('submit').addEventListener('click', e => {
+            const filesToUpload = document.getElementsByClassName('file_multi_video')[0].files
+            let video_link = document.getElementById('url').value
+            let description = document.getElementById('description').value
+            if(video_link != '' && filesToUpload.length != 0)
+                alert("Please, either remove the URL or refresh the page to remove the uploaded video")
+            else if(video_link == '' && filesToUpload.length == 0)
+                alert("Please, either paste the URL or upload the video to process")
+            else if(filesToUpload.length != 0) {
+                var fd = new FormData();
+                fd.append('video', filesToUpload[0])
+                fd.append('description', description)
+                $.ajax({
+                    url: '/other',
+                    type: 'PUT',
+                    data: fd,
+                    contentType: false,
+                    processData: false,
+                    success: function(result) {
+                        // Do something with the result,
+                        console.log(result)
+                    },
+                    error: function(e) {
+                        console.log(e);
+                    },
+                });
+            }
+            else 
+            {
+                var formdata = new FormData();
+                formdata.append('url', video_link)
+                formdata.append('description', description)
+                getUrlVideo(formdata) 
+            }
+        })
 }());      
     
-function getUrlVideo(url) {
+function getUrlVideo(data) {
     $.ajax({
         url:  "/other",
         type: 'POST',
         contentType: false,
         processData: false,
-        data: url,
+        data: data,
         success: function(result) {
             // Do something with the result,
             console.log(result)
@@ -56,11 +75,112 @@ function getUrlVideo(url) {
     });
 }
 
-function getGoogleSearch() {
-    let key = "AIzaSyDg8TOwxGbCQ_KiVdGHSs36VfOlU84SVqc";
-    let qry = "nebulas"; 
-    let cx  = "";
-    let fileType = "png,jpg";
-    let searchType = "image";
-    let url = "https://www.googleapis.com/customsearch/v1?key=" +key+ "&amp;cx=" +cx+ "&amp;q=" +qry+"&amp;fileType="+fileType+"&amp;searchType="+searchType+"&amp;alt=json";
+function google(){
+    var xhr = new XMLHttpRequest();
+    let google = "https://www.google.com/searchbyimage?"
+    let image_link = "https://i.ebayimg.com/images/g/mwQAAOSw1Tdb80~p/s-l500.jpg"
+    let url_specify = "site=search&image_url="
+    xhr.open("GET", google + url_specify + image_link, true);
+    xhr.onload = function (e) {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            let html = xhr.responseText;
+            let description = html.substring(html.indexOf('title="Search" value="') + 22, html.indexOf(' aria-label="Search"') - 1)
+            getEbayDescription(google + "q=" + description + "+ebay&" + url_specify + image_link)
+            console.log(description)
+        } else {
+            console.error(xhr.statusText);
+        }
+    }
+    };
+    xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+    };
+    xhr.send(null); 
+}
+
+function getEbayDescription(url) {
+    console.log(url)
+    let ebay_descriptions = ''
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onload = function (e) {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            let html = xhr.responseText;
+            var allProducts = new Set();
+            let index = 1, ebay_description;
+            //console.log(html)
+            while(index >= 0){
+                index = html.indexOf("https://www.ebay.com/itm/", index + 1)
+                //console.log(index)
+                if(index >= 0){
+                    ebay_description = html.substring(index + 25, html.indexOf("/", index + 26))
+                    //console.log(ebay_description)
+                    if(!allProducts.has(ebay_description))
+                    {
+                        allProducts.add(ebay_description)
+                        //console.log(ebay_description)
+                        ebay_description = ebay_description.split("-").join(" ");
+                        ebay_descriptions = `${ebay_descriptions} - ${ebay_description} `
+                    }
+                }
+            }
+
+            console.log(ebay_descriptions)
+            getEbayProducts(ebay_descriptions.toLowerCase())
+
+        } else {
+            console.error(xhr.statusText);
+        }
+    }
+    };
+    xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+    };
+    xhr.send(null); 
+}
+
+function getEbayProducts(data) {
+    var formdata = new FormData();
+    formdata.append('list', data)
+    $.ajax({
+        url:  "/get_ebay_products",
+        type: 'POST',
+        contentType: false,
+        processData: false,
+        data: formdata,
+        success: function(result) {
+            // Do something with the result,
+            var allResults = Object.values(result)[0]
+            console.log(allResults)
+            var row = document.createElement('div')
+            row.className = 'row list'
+            for(var i = 0; i < allResults.length; i++)
+            {
+                var col = document.createElement('div')
+                col.className = "col-md-3 postCover"
+                var divCover = document.createElement('div')
+                divCover.className = "cover"
+                var image = document.createElement('img')
+                image.src = allResults[i].url
+                var divName = document.createElement('div')
+                divName.className = "name"
+                divName.innerHTML = allResults[i].title.toLowerCase()
+                var divPrice = document.createElement('div')
+                divPrice.className = "coverPrice"
+                divPrice.innerHTML = "$ " + allResults[i].price
+                divCover.appendChild(divName)
+                divCover.appendChild(divPrice)
+                col.appendChild(image)
+                col.appendChild(divCover)
+                row.appendChild(col)
+            }
+
+            document.getElementById('ebayPosts').appendChild(row)
+        },
+        error: function(e) {
+            console.log(e);
+        },
+    });
 }
