371f0f2ced70ea9d62fd7d39614737886113df68
Jamshidbek Mirzakhalov <mirzakhalov@techs-MBP-3.home>
2020-03-08 05:18:26 -0400

    added POST request stuff

--- search.py
+++ search.py
@@ -2,6 +2,7 @@
 from fuzzywuzzy import fuzz
 import json
 import pyrebase
+import requests
 
 config = json.loads(open('secret/config.json').read())
 firebase = pyrebase.initialize_app(config)
@@ -9,6 +10,11 @@ firebase = pyrebase.initialize_app(config)
 # # build paths
 db = firebase.database()
 stg = firebase.storage()
+
+API_ENDPOINT = "https://api.imgur.com/3/image"
+  
+# your API key here 
+headers = {'Authorization': 'Bearer 120517490a8a9e9aa415106ce3694339da0c90c9'}
 
 
 class Search:
@@ -42,6 +48,7 @@ class Search:
         frames = db.child(filename + "/frames/").get()
         max_corr = 0
         frame_id = 0
+        urls = []
         for frame in frames.each():
             corr = fuzz.token_sort_ratio(input, frame.val()['caption'])
             if corr > max_corr:
@@ -50,14 +57,26 @@ class Search:
         
         print(frame_id)
         # retrieve the image
-        ebay = db.child(filename + "/ebay/").get()
-        for stuff in ebay.each():
-            for key, item in ebay.val().items():
-                if str(frame_id) in ebay.val()[key]:
-                    boxes = ebay.val()[key][str(frame_id)]
-                    print(boxes)
-                    # generate boxes and upload
+        # ebay = db.child(filename + "/ebay/").get()
+        # for stuff in ebay.each():
+        #     for key, item in ebay.val().items():
+        #         if str(frame_id) in ebay.val()[key]:
+        #             boxes = ebay.val()[key][str(frame_id)]
+        #             print(boxes)
+        #             # generate boxes and upload
 
+                    # send post requests
+        data = {
+            "image": None
+        }
+
+        session = requests.Session()
+        response = session.post(API_ENDPOINT, headers=headers, data=data)
+
+        if(response.statusCode == 200):
+            urls.append(response.json()['data']['link'])
+
+        
 
         # return the second of the video
         return 2*frame_id, urls
