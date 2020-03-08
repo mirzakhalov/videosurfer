84f14460070e42472d1f49a4fd25b20597a2fad4
Jamshidbek Mirzakhalov <mirzakhalov@techs-MBP-3.home>
2020-03-08 03:46:49 -0400

    started search functions

---
+++ search.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+class Search:
+    def __init__(self):
+        pass
+    
+
+
+    def search_transcript(self, input):
+        pass
+
+    
+
+    def search_caption(self, input):
+        pass
+    
+
+    def search_shopping(self, input):
+        pass
